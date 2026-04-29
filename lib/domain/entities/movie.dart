5914f47edc6fcfc6e063f4267b3a6dd245d99dac
Zwel Htet <zwel@mobiointeractive.com>
2026-04-29 14:12:45 +0630

    [new] movie entity

---
+++ lib/domain/entities/movie.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  final int id;
+  final String title;
+  final String? posterPath;
+  final String? backdropPath;
+  final String overview;
+  final double voteAverage;
+  final int voteCount;
+  final String? releaseDate;
+  final List<int> genreIds;
+  final double popularity;
+  final String? originalLanguage;
+
+  const Movie({
+    required this.id,
+    required this.title,
+    this.posterPath,
+    this.backdropPath,
+    required this.overview,
+    required this.voteAverage,
+    required this.voteCount,
+    this.releaseDate,
+    required this.genreIds,
+    required this.popularity,
+    this.originalLanguage,
+  });
+
+  String get releaseYear {
+    if (releaseDate == null || releaseDate!.isEmpty) return '';
+    return releaseDate!.split('-').first;
+  }
+
+  @override
+  List<Object?> get props => [
+    id,
+    title,
+    posterPath,
+    backdropPath,
+    overview,
+    voteAverage,
+    voteCount,
+    releaseDate,
+    genreIds,
+    popularity,
+    originalLanguage,
+  ];
+}
